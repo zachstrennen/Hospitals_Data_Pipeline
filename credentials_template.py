e1a2a8063f71cfce143ad58ed5b70a93112fbc9a
Zach Strennen <zachstrennen@zachs-mbp.myfiosgateway.com>
2023-12-29 16:41:34 -0500

    Add hostname to template

--- credentials_template.py
+++ credentials_template.py
@@ -1,6 +1,14 @@
 # Create a copy of this file and name it credentials.py
 # Replace the strings with applicable credentials
 # .gitignore will prevent these credentials from being pushed
+def get_hostname():
+    """
+    Separate function to store hostname string
+    :return: Database name
+    """
+    return "HOSTNAME"
+
+
 def get_db():
     """
     Separate function to store database name string
